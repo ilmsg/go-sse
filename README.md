7d0f2a24e07ac0b39a3baa7c3e13a90cca11a7bd
Eak Netpanya <eak.netpanya@gmail.com>
2025-03-31 03:38:31 +0700

    init project

--- README.md
+++ README.md
@@ -1,2 +1,7 @@
 # go-sse
+
 How to Implement Server-Sent Events in Go
+
+Example from 
+https://www.youtube.com/watch?v=nvijc5J-JAQ
+
---
+++ main.go
@@ -0,0 +1,68 @@
+package main
+
+import (
+	"fmt"
+	"log"
+	"net/http"
+	"time"
+
+	"github.com/shirou/gopsutil/v4/cpu"
+	"github.com/shirou/gopsutil/v4/mem"
+)
+
+func main() {
+	http.Handle("/", http.FileServer(http.Dir("./static")))
+	http.HandleFunc("/events", sseHandler)
+
+	if err := http.ListenAndServe(":3010", nil); err != nil {
+		log.Fatalf("unable to start server: %s", err.Error())
+	}
+}
+
+func sseHandler(w http.ResponseWriter, r *http.Request) {
+	w.Header().Set("Content-Type", "text/event-stream")
+	w.Header().Set("Cache-Control", "no-cache")
+	w.Header().Set("Connection", "keep-alive")
+	w.Header().Set("Access-Control-Allow-Origin", "*")
+
+	memT := time.NewTicker(time.Second)
+	defer memT.Stop()
+
+	cpuT := time.NewTicker(time.Second)
+	defer cpuT.Stop()
+
+	clientGone := r.Context().Done()
+	rc := http.NewResponseController(w)
+	for {
+		select {
+		case <-clientGone:
+			fmt.Println("client has disconnected")
+		case <-memT.C:
+			m, err := mem.VirtualMemory()
+			if err != nil {
+				log.Printf("unable to get mem: %s", err.Error())
+				return
+			}
+
+			if _, err := fmt.Fprintf(w, "event:mem\ndata:Total: %d, Used: %d, Perc: %.2f%%\n\n", m.Total, m.Used, m.UsedPercent); err != nil {
+				log.Printf("unable to write: %s", err.Error())
+				return
+			}
+
+			rc.Flush()
+		case <-cpuT.C:
+			c, err := cpu.Times(false)
+			if err != nil {
+				log.Printf("unable to get cpu: %s", err.Error())
+				return
+			}
+
+			if _, err := fmt.Fprintf(w, "event:cpu\ndata:User: %.2f, Sys: %.2f, Idle: %.2f\n\n", c[0].User, c[0].System, c[0].Idle); err != nil {
+				log.Printf("unable to write: %s", err.Error())
+				return
+			}
+
+			rc.Flush()
+		}
+	}
+}
